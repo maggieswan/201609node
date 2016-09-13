2d5cb14b171e1901c8a33091454054e2e268ab28
maggie4ever <1245567022@qq.com>
2016-09-13 08:51:16 +0800

    git new

--- 7.file/3.copyfile.js
+++ 7.file/3.copyfile.js
@@ -1,12 +1,12 @@
 
 var  fs=require('fs');
  function copy(src,target){
-    var readf= fs.readFileSync('3.txt',{encoding:'utf8'});
-　　fs.writeFileSync('./4.txt',readf,{encoding:'utf8'});
+    var readf= fs.readFileSync('1.png');
+　　fs.writeFileSync('./2.png',readf)
 
  }
-copy('./3.txt','./4.txt');
-
+//copy('./3.txt','./4.txt');
+copy('./1.png','./2.png');
 
 
 
